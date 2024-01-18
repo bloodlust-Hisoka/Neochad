38507cc3da7a9655c3b4d8c1f67e7e183cf16af0
bloodlust-Hisoka <156512009+bloodlust-Hisoka@users.noreply.github.com>
2024-01-18 09:47:08 +0530

    Update theming.lua

--- lua/User/core/theming.lua
+++ lua/User/core/theming.lua
@@ -1,3 +1,3 @@
-local defaultTheme = require('User.config.config').defaultTheme
+local defaultTheme = 'kanagawa'
 local theme = require('last-color').recall() or defaultTheme
 vim.cmd(('colorscheme %s'):format(theme))
